a58504ceb1745e7f38f1ff5c875a72491e13f3a9
danleech-defra <dan.leech@defra.net>
2023-02-03 09:22:10 +0000

    Layer styling

--- app/assets/javascripts/components/map/layers.js
+++ app/assets/javascripts/components/map/layers.js
@@ -33,11 +33,7 @@ window.flood.maps.layers = {
   },
 
   surfaceWater: (liklihood) => {
-    const dynamicLayers = [
-      '[{"id":2,"name":"RoFSW_Extent_1in30","source":{"type":"mapLayer","mapLayerId":2},"drawingInfo":{"renderer":{"type":"simple","symbol":{"color":[127,151,187,255],"outline":{"color":[0,0,0,0],"width":0.4,"type":"esriSLS","style":"esriSLSNull"},"type":"esriSFS","style":"esriSFSSolid"}}}}]',
-      '[{"id":4,"name":"RoFSW_Extent_1in100","source":{"type":"mapLayer","mapLayerId":4},"drawingInfo":{"renderer":{"type":"simple","symbol":{"color":[127,151,187,255],"outline":{"color":[0,0,0,0],"width":0.4,"type":"esriSLS","style":"esriSLSNull"},"type":"esriSFS","style":"esriSFSSolid"}}}}]',
-      '[{"id":6,"name":"RoFSW_Extent_1in1000","source":{"type":"mapLayer","mapLayerId":6},"drawingInfo":{"renderer":{"type":"simple","symbol":{"color":[127,151,187,255],"outline":{"color":[0,0,0,0],"width":0.4,"type":"esriSLS","style":"esriSLSNull"},"type":"esriSFS","style":"esriSFSSolid"}}}}]'
-    ]
+    const layerIds = [2, 4, 6]
     return new TileLayer({
       ref: `surfaceWater${liklihood}`,
       className: 'defra-map-raster-canvas',
@@ -48,7 +44,7 @@ window.flood.maps.layers = {
         params: {
           'TRANSPARENT': true,
           'FORMAT': 'GIF',
-          'dynamicLayers' : dynamicLayers[liklihood-1]
+          'dynamicLayers' : `[{"id":${layerIds[liklihood - 1]},"source":{"type":"mapLayer","mapLayerId":${layerIds[liklihood - 1]}},"drawingInfo":{"renderer":{"type":"simple","symbol":{"color":[127,151,187,255],"outline":{"width":0,"type":"esriSLS"},"type":"esriSFS","style":"esriSFSSolid"}}}}]`
         }
       }),
       minZoom: 7,
@@ -68,8 +64,8 @@ window.flood.maps.layers = {
         projection: 'EPSG:27700',
         params: {
           'TRANSPARENT': true,
-          'FORMAT': 'GIF'
-          // 'dynamicLayers' : '[{"id":0,"source":{"type":"mapLayer","mapLayerId":0},"drawingInfo":{"renderer":{"type":"uniqueValue","field1":"depth","uniqueValueInfos":[{"description":"","label":"Below 150mm","symbol":{"type":"esriSFS","color":[191,203,221,255],"outline":{"type":"esriSLS","color":[0,0,0,0],"width":0,"style":"esriSLSSolid"},"style":"esriSFSSolid"},"value":"0.00 - 0.15"},{"description":"","label":"150-300mm","symbol":{"type":"esriSFS","color":[191,203,221,255],"outline":{"type":"esriSLS","color":[0,0,0,0],"width":0,"style":"esriSLSSolid"},"style":"esriSFSSolid"},"value":"0.15 - 0.30"},{"description":"","label":"300-600mm","symbol":{"type":"esriSFS","color":[191,203,221,255],"outline":{"type":"esriSLS","color":[0,0,0,0],"width":0,"style":"esriSLSNull"},"style":"esriSFSSolid"},"value":"0.30 - 0.60"},{"description":"","label":"600-900mm","symbol":{"type":"esriSFS","color":[191,203,221,255],"outline":{"type":"esriSLS","color":[0,0,0,0],"width":0,"style":"esriSLSNull"},"style":"esriSFSSolid"},"value":"0.60 - 0.90"},{"description":"","label":"900-1200mm","symbol":{"type":"esriSFS","color":[64,100,154,0],"outline":{"type":"esriSLS","color":[0,0,0,0],"width":0,"style":"esriSLSNull"},"style":"esriSFSSolid"},"value":"0.90 - 1.20"},{"description":"","label":"Over 1200mm","symbol":{"type":"esriSFS","color":[64,100,154,0],"outline":{"type":"esriSLS","color":[0,0,0,0],"width":0,"style":"esriSLSNull"},"style":"esriSFSSolid"},"value":"> 1.20"}]}}}]'
+          'FORMAT': 'GIF',
+          'dynamicLayers' : '[{"id":0,"source":{"type":"mapLayer","mapLayerId":0},"drawingInfo":{"renderer":{"type":"uniqueValue","field1":"depth","uniqueValueInfos":[{"value":"0.00 - 0.15","symbol":{"type":"esriSFS","style":"esriSFSSolid","color":[191,203,221,255],"outline":{"type":"esriSLS","width":0}}},{"value":"0.15 - 0.30","symbol":{"type":"esriSFS","style":"esriSFSSolid","color":[191,203,221,255],"outline":{"type":"esriSLS","width":0}}},{"value":"0.30 - 0.60","symbol":{"type":"esriSFS","style":"esriSFSSolid","color":[127,151,187,255],"outline":{"type":"esriSLS","width":0}}},{"value":"0.60 - 0.90","symbol":{"type":"esriSFS","style":"esriSFSSolid","color":[127,151,187,255],"outline":{"type":"esriSLS","width":0}}},{"value":"0.90 - 1.20","symbol":{"type":"esriSFS","style":"esriSFSSolid","color":[64,100,154,0],"outline":{"type":"esriSLS","width":0}}},{"value":"> 1.20","symbol":{"type":"esriSFS","style":"esriSFSSolid","color":[64,100,154,0],"outline":{"type":"esriSLS","width":0}}}]},"transparency":0}}]'
         }
       }),
       minZoom: 7,
@@ -89,8 +85,8 @@ window.flood.maps.layers = {
         projection: 'EPSG:27700',
         params: {
           'TRANSPARENT': true,
-          'FORMAT': 'GIF'
-          // 'dynamicLayers' : '[{"id":0,"source":{"type":"mapLayer","mapLayerId":0},"drawingInfo":{"renderer":{"type":"uniqueValue","field1":"depth","uniqueValueInfos":[{"value":"0.00 - 0.25","label":"Less than 0.25 m/s","symbol":{"color":[191,203,221,255],"outline":{"color":[0,0,0,0],"width":0,"type":"esriSLS","style":"esriSLSSolid"},"type":"esriSFS","style":"esriSFSSolid"}},{"value":"0.25 - 0.50","label":"0.25 - 0.50 m/s","symbol":{"color":[64,100,154,255],"outline":{"color":[0,0,0,0],"width":0,"type":"esriSLS","style":"esriSLSSolid"},"type":"esriSFS","style":"esriSFSSolid"}},{"value":"0.50 - 1.00","label":"0.50 - 1.00 m/s","symbol":{"color":[64,100,154,255],"outline":{"color":[0,0,0,0],"width":0,"type":"esriSLS","style":"esriSLSSolid"},"type":"esriSFS","style":"esriSFSSolid"}},{"value":"1.00 - 2.00","label":"1.00 - 2.00 m/s","symbol":{"color":[64,100,154,255],"outline":{"color":[0,0,0,0],"width":0,"type":"esriSLS","style":"esriSLSSolid"},"type":"esriSFS","style":"esriSFSSolid"}},{"value":"> 2.00","label":"Over 2.00 m/s","symbol":{"color":[64,100,154,0],"outline":{"color":[0,0,0,0],"width":0,"type":"esriSLS","style":"esriSLSSolid"},"type":"esriSFS","style":"esriSFSSolid"}}]},"showLabels":false}}]'
+          'FORMAT': 'GIF',
+          'dynamicLayers' : '[{"id":0,"source":{"type":"mapLayer","mapLayerId":0},"drawingInfo":{"renderer":{"type":"uniqueValue","field1":"velocity","uniqueValueInfos":[{"value":"0.00 - 0.25","symbol":{"color":[191,203,221,255],"outline":{"width":0,"type":"esriSLS"},"type":"esriSFS","style":"esriSFSSolid"}},{"value":"0.25 - 0.50","symbol":{"color":[64,100,154,255],"outline":{"width":0,"type":"esriSLS"},"type":"esriSFS","style":"esriSFSSolid"}},{"value":"0.50 - 1.00","symbol":{"color":[64,100,154,255],"outline":{"width":0,"type":"esriSLS"},"type":"esriSFS","style":"esriSFSSolid"}},{"value":"1.00 - 2.00","symbol":{"color":[64,100,154,255],"outline":{"width":0,"type":"esriSLS"},"type":"esriSFS","style":"esriSFSSolid"}},{"value":"> 2.00","symbol":{"color":[64,100,154,255],"outline":{"width":0,"type":"esriSLS"},"type":"esriSFS","style":"esriSFSSolid"}}]}}}]'
         }
       }),
       minZoom: 7,
@@ -110,7 +106,7 @@ window.flood.maps.layers = {
         params: {
           'TRANSPARENT': true,
           'FORMAT': 'GIF',
-          'dynamicLayers' : `[{"id":0,"name":"Risk_of_Flooding_from_Rivers_and_Sea","source":{"type":"mapLayer","mapLayerId":0},"drawingInfo":{"renderer":{"type":"uniqueValue","field1":"prob_4band","uniqueValueInfos":[{"value":"High","label":"High","symbol":{"color":[127,151,187,255],"outline":{"color":[0,0,0,0],"width":0,"type":"esriSLS","style":"esriSLSSolid"},"type":"esriSFS","style":"esriSFSSolid"}},{"value":"Medium","label":"Medium","symbol":{"color":[${liklihood > 1 ? '127,151,187,255' : '0,0,0,0'}],"outline":{"color":[0,0,0,0],"width":0,"type":"esriSLS","style":"esriSLSSolid"},"type":"esriSFS","style":"esriSFSSolid"}},{"value":"Low","label":"Low","symbol":{"color":[${liklihood > 2 ? '127,151,187,255' : '0,0,0,0'}],"outline":{"color":[0,0,0,0],"width":0,"type":"esriSLS","style":"esriSLSSolid"},"type":"esriSFS","style":"esriSFSSolid"}},{"value":"Very Low","label":"Very Low","symbol":{"color":[0,0,0,0],"outline":{"color":[0,0,0,0],"width":0,"type":"esriSLS","style":"esriSLSSolid"},"type":"esriSFS","style":"esriSFSSolid"}}]},"showLabels":false}}]`
+          'dynamicLayers': `[{"id":0,"source":{"type":"mapLayer","mapLayerId":0},"drawingInfo":{"renderer":{"type":"uniqueValue","field1":"prob_4band","uniqueValueInfos":[{"value":"High","symbol":{"color":[127,151,187,255],"outline":{"width":0,"type":"esriSLS"},"type":"esriSFS","style":"esriSFSSolid"}},{"value":"Medium","symbol":{"color":[${liklihood > 1 ? '127,151,187,255' : '0,0,0,0'}],"outline":{"width":0,"type":"esriSLS"},"type":"esriSFS","style":"esriSFSSolid"}},{"value":"Low","symbol":{"color":[${liklihood > 2 ? '127,151,187,255' : '0,0,0,0'}],"outline":{"width":0,"type":"esriSLS"},"type":"esriSFS","style":"esriSFSSolid"}},{"value":"Very Low","symbol":{"color":[0,0,0,0],"outline":{"width":0,"type":"esriSLS"},"type":"esriSFS","style":"esriSFSSolid"}}]}}}]`
         }
       }),
       minZoom: 7,
